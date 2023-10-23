46015f7bce003081846b1942b4af9b3a53033dbb
LingZhouChen <lingzhouc@gmail.com>
2023-10-23 10:32:03 -0400

    complete lab

---
+++ lib/app.py
@@ -0,0 +1,14 @@
+# run this code by:
+    # python lib/app.py
+
+# print("Hello world!")
+# print("Hello sun!")
+# print("Hello sky!")
+
+# write full paragraph and don't need a newline char after every sentence
+    # expect: Hello world! Hello sun!!! Hello sky!!!!
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sky!", end="!!!\n")
+
+print("Hello World! Pass this test, please.")
